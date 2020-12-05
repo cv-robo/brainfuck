7e615006ba6c4cf1dec64e2f6f3f00d3cd447342
cv-robo <lzhynt@tom.com>
2020-12-05 22:15:30 +0800

    format

--- brainfuck.c
+++ brainfuck.c
@@ -1,59 +1,53 @@
 #include "brainfuck.h"
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-void bf_run(char *program, size_t memory_size)
-{
+void bf_run(char *program, size_t memory_size) {
     size_t program_size = strlen(program);
     char *stack = (char *)malloc(program_size);
     char *memory = (char *)calloc(memory_size, sizeof(char));
     size_t pc = 0, sp = 0, ptr = memory_size / 2;
-    for (; pc < program_size; pc++)
-    {
-        if (!sp && program[pc] == ']')
-        {
+    for (; pc < program_size; pc++) {
+        if (!sp && program[pc] == ']') {
             printf("sp is zero.\n");
             break;
         }
-        switch (program[pc])
-        {
-        case '>':
-            ptr++;
-            break;
-        case '<':
-            ptr--;
-            break;
-        case '+':
-            memory[ptr]++;
-            break;
-        case '-':
-            memory[ptr]--;
-            break;
-        case ',':
-            memory[ptr] = getchar();
-            getchar();
-            break;
-        case '.':
-            putchar(memory[ptr]);
-            break;
-        case '[':
-            stack[sp] = pc;
-            sp++;
-            break;
-        case ']':
-            pc = memory[ptr] ? stack[sp - 1] : pc;
-            sp -= !memory[ptr];
-            break;
+        switch (program[pc]) {
+            case '>':
+                ptr++;
+                break;
+            case '<':
+                ptr--;
+                break;
+            case '+':
+                memory[ptr]++;
+                break;
+            case '-':
+                memory[ptr]--;
+                break;
+            case ',':
+                memory[ptr] = getchar();
+                getchar();
+                break;
+            case '.':
+                putchar(memory[ptr]);
+                break;
+            case '[':
+                stack[sp] = pc;
+                sp++;
+                break;
+            case ']':
+                pc = memory[ptr] ? stack[sp - 1] : pc;
+                sp -= !memory[ptr];
+                break;
         }
-        if (ptr < 0 || ptr >= memory_size)
-        {
+        if (ptr < 0 || ptr >= memory_size) {
             printf("pointer out of range.\n");
             break;
         }
-        if (sp >= program_size)
-        {
+        if (sp >= program_size) {
             printf("sp out of range.\n");
             break;
         }
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+
 #include "brainfuck.h"
 
-int main(void)
-{
+int main(void) {
     char program[1024];
     scanf("%s", program);
     getchar();
